60250e1d55cb8e77b0fbd6c3335a8e8218c83294
RahulAmbesange <rahulambesange7@gmail.com>
2024-07-19 15:28:55 +0530

    geminiclone

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import SideBar from './Components/SideBar/SideBar'
+import Main from './Components/Main/Main'
+
+function App() {
+  
+  return (
+    <>
+      <SideBar />
+      <Main/>
+    </>
+  )
+}
+
+export default App
